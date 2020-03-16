49b60b0bc40699ad700ddaaa3ef8c65485468025
Puneet <puneettiwari61@gmail.com>
2020-03-16 15:00:18 +0530

    cors1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 // var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose')
-var cors = require('cors')
+// var cors = require('cors')
 
 // configure dotenv
 require('dotenv').config();
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGO_URI,
     console.log('connected', err? err: true);
   }
 )
-app.use(cors())
+// app.use(cors())
 
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
